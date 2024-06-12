bed009b2d0d17033c4ae56c32e6e1b7470f55b68
Otavio Marques <marquesotavio251204@gmail.com>
2024-06-11 21:02:16 -0300

    mudanças login

--- src/pages/login.js
+++ src/pages/login.js
@@ -3,33 +3,28 @@ import { z } from "zod";
 import './login.css';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useNavigate } from "react-router-dom";
 
 const LoginSchema = z.object({
-    email: z.string().email(),
-    password: z.string().min(8)
+    email: z.string().email({message: 'Email invalido'}),
+    password: z.string().min(8, {message: 'Senha com pelo menos 8 caracteres'})
 });
 
 const Login = () => {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const navigate = useNavigate();
 
     const submitForm = (e) => {
         e.preventDefault();
 
         try {
             LoginSchema.parse({ email, password });
-            toast.success('VAMOOOOO');
+            navigate('/information-api');
         } catch (err) {
             err.errors.forEach(error => {
-                if(error.message === 'Invalid email'){
-                    toast.error('Invalid email');
-                }
-                else if(error.message === 'String must contain at least 8 character(s)'){
-                    toast.error('Password must be at least 8 characters');
-                }else {
-                    toast.error('Invalid credentials');
-                }
+                toast.error(error.message);
             });
         }
 
@@ -41,7 +36,7 @@ const Login = () => {
             <form onSubmit={submitForm}>
                 <input type="text" placeholder='Digite seu email' value={email} onChange={(e) => setEmail(e.target.value)} />
                 <input type="password" placeholder='Digite sua senha' value={password} onChange={(e) => setPassword(e.target.value)} />
-                <button type="submit">Enviar</button>
+                <button type="submit" className='btnEnviar'>Enviar</button>
             </form>
             <ToastContainer />
         </div>
